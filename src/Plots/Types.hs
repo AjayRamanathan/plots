5a03404737a1326f9447727e1c433e630562fc35
Ajay Ramanathan <ajay.010293@gmail.com>
2015-06-11 14:02:25 +0530

    added Plotfn, PointMap etc. to Types.hs

--- src/Plots/Types.hs
+++ src/Plots/Types.hs
@@ -84,6 +84,13 @@ module Plots.Types
   , _Plot'
   , unPlot'
   , appPlot'
+
+  ,DiaR2
+  , PlotFn
+  , PointMap(..)
+  , PlotData(..)
+  , compose
+  , flipMap
   ) where
 
 -- import           Control.Lens          as L hiding (transform, ( # ), (|>))
@@ -93,13 +100,17 @@ import           Data.Monoid.Recommend
 import           Data.Typeable
 import           Diagrams.BoundingBox
 import           Diagrams.Prelude      as D hiding (view)
-import Data.Foldable (Foldable)
-
+import           Data.Foldable (Foldable)
+import           Data.Maybe
 import           Linear
 import           Plots.Themes
 import           Plots.Utils
+import           Diagrams.Backend.Cairo (B)
+
 
 type family B a :: *
+
+type DiaR2 = Diagram B R2
 
 type instance B (QDiagram b v n m) = b
 
@@ -468,3 +479,44 @@ instance HasPlotStyle (PropertiedPlot p b) b where
 
 instance Qualifiable (PropertiedPlot p b) where
   n .>> p = over plotName (n .>>) p
+
+type PlotFn = PointMap Double -> PointMap Double -> [DiaR2]
+
+-- | mapping between two coordinate system
+data PointMap a = PointMap
+    { runMap :: a -> Maybe a
+    , domain :: (a, a)
+    }
+
+-- | combine two point maps
+compose :: PointMap a -> PointMap a -> PointMap (a,a)
+compose (PointMap m1 (lo,hi)) (PointMap m2 (lo',hi')) = PointMap mapFn dom
+  where
+    mapFn (x,y) = do x' <- m1 x
+                     y' <- m2 y
+                     return (x', y')
+    dom = ((lo, lo'), (hi, hi'))
+{-# INLINE compose #-}
+
+flipMap :: PointMap Double -> PointMap Double
+flipMap (PointMap f (lo, hi)) = PointMap mapFn (lo, hi)
+  where
+    mapFn x = do x' <- f x
+                 return (hi' - x' + lo')
+    lo' = fromJust.f $ lo
+    hi' = fromJust.f $ hi
+
+class PlotData m a where
+    getValues :: m a -> [Double]
+
+instance PlotData [] Int where
+    getValues = map fromIntegral
+
+instance PlotData [] Double where
+    getValues = id
+
+instance PlotData Maybe a where
+    getValues _ = [1.0..]
+
+instance PlotData [] String where
+    getValues _ = [1.0..]
