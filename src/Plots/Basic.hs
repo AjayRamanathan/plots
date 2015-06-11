5bf2f3bec9cc95b326c4f1423826c1101ebbf589
Ajay Ramanathan <ajay.010293@gmail.com>
2015-06-11 18:23:53 +0530

    added a seperate render for plotdata

---
+++ src/Plots/Basic.hs
@@ -0,0 +1,158 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Plots.Basic
+    ( with
+    -- * General plot options
+    , PlotOpt
+    , x
+    , y
+    , height
+    , width
+    , title
+    , file
+    , xlab
+    , ylab
+    , xNames
+    , yNames
+    , xLabelOpt
+    , yLabelOpt
+    , extra
+
+    -- * Line Options
+    , showPoint 
+
+    , barPlot
+    , barPlot'
+    , linePlot
+    , linePlot'
+
+    , heatmap
+    , heatmap'
+    ) where
+
+import Control.Lens
+import Data.Default
+import Data.Monoid (mempty)
+import Data.Void (Void)
+
+import Diagrams.Backend.Cairo
+import Diagrams.Prelude (SizeSpec2D(..), with, (===), rect, (<>), center)
+import Plots
+
+data PlotOpt datX datY o = PlotOpt
+    { _plotOptX :: [datX]
+    , _plotOptY :: [datY]
+    , _plotOptHeight :: Double
+    , _plotOptWidth :: Double
+    , _plotOptXlab :: String
+    , _plotOptYlab :: String
+    , _plotOptXNames :: [String]
+    , _plotOptYNames :: [String]
+    , _plotOptXLabelOpt :: LabelOpt
+    , _plotOptYLabelOpt :: LabelOpt
+    , _plotOptTitle :: String
+    , _plotOptFile :: String
+    , _plotOptExtra :: o
+    }
+
+makeFields ''PlotOpt
+
+instance Default o => Default (PlotOpt datX datY o) where
+    def = PlotOpt
+        { _plotOptX = []
+        , _plotOptY = []
+        , _plotOptHeight = 480
+        , _plotOptWidth = 480
+        , _plotOptXlab = ""
+        , _plotOptYlab = ""
+        , _plotOptXNames = []
+        , _plotOptYNames = []
+        , _plotOptXLabelOpt = def
+        , _plotOptYLabelOpt = def
+        , _plotOptTitle = ""
+        , _plotOptFile = "plot.png"
+        , _plotOptExtra = def
+        }
+
+data LinePlotOpt = LinePlotOpt 
+    { _linePlotOptShowPoint :: Bool
+    }
+
+makeFields ''LinePlotOpt
+
+instance Default LinePlotOpt where
+    def = LinePlotOpt
+        { _linePlotOptShowPoint = False
+        }
+
+barPlot :: PlotOpt Double Void BarOpt -> IO ()
+barPlot opt = renderCairo (opt^.file) (Dims w h) . barPlot' $ opt
+  where
+    w = opt^.width
+    h = opt^.height
+
+barPlot' :: PlotOpt Double Void BarOpt -> DiaR2
+barPlot' opt = text' 0.3 (opt^.title) === plot
+  where
+    plot = showPlot $ area <+ (b, BL)
+    area = plotArea (5.5*w/h) 5.5 (yAxis, def, def, xAxis)
+    b = bars xs Nothing (opt^.extra)
+    yAxis = indexAxis (length xs) (opt^.yNames) 0.2 def
+    xAxis = realAxis (minimum xs, maximum xs) 0.2 def
+    xs = opt^.x
+    w = opt^.width
+    h = opt^.height
+
+linePlot :: PlotOpt Double Double LinePlotOpt -> IO ()
+linePlot opt = renderCairo (opt^.file) (Dims w h) . linePlot' $ opt
+  where
+    w = opt^.width
+    h = opt^.height
+
+linePlot' :: PlotOpt Double Double LinePlotOpt -> DiaR2
+linePlot' opt = text' 0.3 (opt^.title) === plot 
+  where
+    plot | opt^.extra.showPoint = showPlot $ area <+ (ps, BL) <+ (l, BL)
+         | otherwise = showPlot $ area <+ (l, BL)
+    area = plotArea (5.5*w/h) 5.5 (yAxis, def, def, xAxis)
+    xAxis | null xs = indexAxis (length ys) (opt^.xNames) 0.2 def
+          | otherwise = realAxis (minimum xs, maximum xs) 0.2 def
+    yAxis = realAxis (minimum ys, maximum ys) 0.2 def
+    l | null xs = line Nothing ys def
+      | otherwise = line xs ys def
+    ps | null xs = points Nothing ys def
+       | otherwise = points xs ys def
+    xs = opt^.x
+    ys = opt^.y
+    w = opt^.width
+    h = opt^.height
+{-# INLINE linePlot' #-}
+
+heatmap :: PlotOpt [Double] Void HeatmapOpt -> IO ()
+heatmap opt = renderCairo (opt^.file) (Dims w h) . heatmap' $ opt
+  where
+    w = opt^.width
+    h = opt^.height
+
+heatmap' :: PlotOpt [Double] Void HeatmapOpt -> DiaR2
+heatmap' opt = text' 0.3 (opt^.title) === plot
+  where
+    plot = rect (w'+1) (h'+1) <> center (showPlot $ area <+ (heat,BL))
+    area = plotArea w' h' (rmAxisDiag yAxis, def, def, rmAxisDiag xAxis)
+    xAxis = indexAxis c (opt^.xNames) (w' / fromIntegral c / 2) $
+        with & labelOpt .~ opt^.xLabelOpt
+    yAxis = indexAxis r (reverse $ opt^.yNames) (h' / fromIntegral r / 2) $
+        with & labelOpt .~ opt^.yLabelOpt
+    heat = Diagrams.Plots.heatmap xs $ opt^.extra
+    xs = opt^.x
+    r = length xs
+    c = length $ head xs
+    w = opt^.width
+    h = opt^.height
+    w' = h' * w / h
+    h' = 5.5
+    rmAxisDiag (AxisFn a) = AxisFn $ fmap (\f -> axisDiag .~ mempty $ f) a
+{-# INLINE heatmap' #-}
---
+++ src/Plots/PlotArea.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Plots.PlotArea 
+    ( P(..)
+    , PlotArea
+    , plotAreaWidth
+    , plotAreaHeight
+    , plotAreaLeft
+    , plotAreaRight
+    , plotAreaTop
+    , plotAreaBottom
+    , plotAreaBackground
+    , plotArea
+    , showPlot
+    , placeOn
+    , (<+)
+    ) where
+
+import Diagrams.Prelude hiding (rotation)
+import Control.Lens (makeLenses, (^.))
+
+import Plots.Axis
+import Plots.Types
+import Plots.Utils (text')
+
+-- | how to align a plot to the plot area
+data P = BL  -- Bottom Left
+       | TL  -- Top Left
+       | TR  -- Top Right
+       | BR  -- Bottom Right
+
+data PlotArea = PlotArea
+    { _plotAreaWidth :: !Double
+    , _plotAreaHeight :: !Double
+    , _plotAreaPlots :: ![DiaR2]
+    , _plotAreaLeft :: !Axis -- ^ point map for left axis
+    , _plotAreaTop :: !Axis -- ^ point map for top axis
+    , _plotAreaRight :: !Axis -- ^ point map for right axis
+    , _plotAreaBottom :: !Axis -- ^ point map for bottom axis
+    , _plotAreaBackground :: !DiaR2
+    }
+
+makeLenses ''PlotArea
+
+-- | construct a plot area
+plotArea :: Double  -- ^ width
+         -> Double  -- ^ height
+         -> (AxisFn, AxisFn, AxisFn, AxisFn) -- ^ axes: left, top, right, bottom
+         -> PlotArea
+plotArea w h (l, t, r, b) = PlotArea w h [] lAxis tAxis rAxis bAxis background
+  where
+    lAxis = makeAxis l h
+    tAxis = makeAxis t w
+    rAxis = makeAxis r h
+    bAxis = makeAxis b w
+    background = lwL 0 $ moveTo ((w/2) ^& (h/2)) $ rect w h
+
+showPlot :: PlotArea -> DiaR2
+showPlot (PlotArea w h ps l t r b bgr) = mconcat 
+    [ drawAxis 'l' l
+    , translateY h . drawAxis 't' $ t
+    , translateX w . drawAxis 'r' $ r
+    , drawAxis 'b' b
+    , mconcat ps
+    , bgr
+    ]
+
+drawAxis :: Char -> Axis -> DiaR2
+drawAxis p a
+    | p == 'l' = (reflectX . rotateBy (1/4)) axis'
+              <> mconcat ( flip map labels $ \((x,y), label) -> 
+                    alignedText 1 0.5 label # rotateBy r
+                                            # fontSizeO fontsize
+                                            # moveTo ((y+dy) ^& (x+dx)) )
+    | p == 't' = reflectY axis'
+              <> mconcat ( flip map labels $ \((x,y), label) ->
+                    text' fontsize label # rotateBy r
+                                         # moveTo ((x+dx) ^& (-y-dy)) )
+    | p == 'r' = rotateBy (1/4) axis'
+              <> mconcat ( flip map labels $ \((x,y), label) ->
+                    alignedText 0 0.5 label # rotateBy r
+                                            # fontSizeO fontsize
+                                            # moveTo ((-y-dy) ^& (x+dx)) )
+    | p == 'b' = axis'
+              <> mconcat ( flip map labels $ \((x,y), label) -> 
+                    let t | r == 0 = text label
+                          | otherwise = alignedText 1 0.5 label
+                    in t # rotateBy r
+                         # fontSizeO fontsize
+                         # moveTo ((x+dx) ^& (y+dy)) )
+    | otherwise = undefined
+  where
+    axis' = a^.axisDiag
+    labels = a^.axisLabels
+    dx = a^.axisLabelOpt^.offsetX
+    dy = a^.axisLabelOpt^.offsetY
+    fontsize = a^.axisLabelOpt^.size
+    r = a^.axisLabelOpt^.rotation
+{-# INLINE drawAxis #-}
+
+placeOn :: (PlotFn, P) -> PlotArea -> PlotArea
+placeOn (pltFn, p) area = plotAreaPlots %~ (mconcat plt:) $ area
+  where
+    plt = case p of
+        BL -> pltFn bMap lMap
+        TL -> pltFn tMap lMap
+        TR -> pltFn tMap rMap
+        BR -> pltFn bMap rMap
+    lMap = area^.plotAreaLeft^.axisMap
+    bMap = area^.plotAreaBottom^.axisMap
+    tMap = area^.plotAreaTop^.axisMap
+    rMap = area^.plotAreaRight^.axisMap
+
+(<+) :: PlotArea -> (PlotFn, P) -> PlotArea
+infixl 1 <+
+(<+) = flip placeOn
