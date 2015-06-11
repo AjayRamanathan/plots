16a16836577d44e23bb10d599c51020c8de22088
Ajay Ramanathan <ajay.010293@gmail.com>
2015-06-11 13:55:16 +0530

    added function to Utils

--- src/Plots/Utils.hs
+++ src/Plots/Utils.hs
@@ -5,7 +5,15 @@
 {-# LANGUAGE BangPatterns          #-}
 {-# LANGUAGE FlexibleContexts      #-}
 
-module Plots.Utils where
+module Plots.Utils
+    ( projection
+    , autoSteps
+    , linearMap
+    , linearMapBound
+    , hasNaN
+    , text'
+    ) where
+
 
 import Control.Lens.Internal.Fold
 import Data.Foldable
@@ -14,7 +22,15 @@ import Control.Lens.Internal
 import Data.Profunctor.Unsafe
 import Data.Monoid.Recommend
 import Linear
-import Diagrams.Prelude hiding (diff)
+import Data.Ord (comparing)
+import Data.Function
+
+import Diagrams.Prelude
+import Graphics.SVGFonts.ReadFont
+import Diagrams.Backend.Cairo.Text
+
+import Data.List
+import Plots.Types
 
 -- | @enumFromToN a b n@ calculates a list from @a@ to @b@ in @n@ steps.
 enumFromToN :: Fractional n => n -> n -> Int -> [n]
@@ -96,3 +112,74 @@ pathFromVertices = fromVertices
 --   where
 --     makeCommitted (Recommend a) = Commit a
 --     makeCommitted c             = c
+
+-- | project a 3d point to 2d
+projection :: (Double, Double, Double)  -- ^ position of camera
+           -> (Double, Double, Double)  -- ^ orientation of camera
+           -> (Double, Double, Double)  -- ^ viewer's position
+           -> (Double, Double, Double)  -- ^ 3d point to be projected
+           -> (Double, Double)
+projection (cx',cy',cz') (θx,θy,θz) (ex,ey,ez) (ax,ay,az) = (bx, by)
+  where
+    bx = ez / dz * dx - ex
+    by = ez / dz * dy - ey
+    dx = cy * (sz * y + cz * x) - sy * z
+    dy = sx * (cy * z + sy * (sz * y + cz * x)) + cx * (cz * y - sz * x)
+    dz = cx * (cy * z + sy * (sz * y + cz * x)) - sx * (cz * y - sz * x)
+    x = ax - cx'
+    y = ay - cy'
+    z = az - cz'
+    sx = sin θx
+    sy = sin θy
+    sz = sin θz
+    cx = cos θx
+    cy = cos θy
+    cz = cos θz
+
+chooseStep :: RealFloat a => a -> (a,a) -> Rational
+chooseStep nsteps (x1,x2) = minimumBy (comparing proximity) stepVals
+  where
+    delta = x2 - x1
+    mult  = 10 ^^ ((floor $ logBase 10 $ delta / nsteps)::Integer)
+    stepVals = map (mult*) [0.1,0.2,0.25,0.5,1.0,2.0,2.5,5.0,10,20,25,50]
+    proximity x = abs $ delta / realToFrac x - nsteps
+{-# INLINE chooseStep #-}
+
+-- | Given a target number of values, and a list of input points,
+--   find evenly spaced values from the set {1*X, 2*X, 2.5*X, 5*X} (where
+--   X is some power of ten) that evenly cover the input points.
+autoSteps :: Int -> (Double, Double) -> (Rational, Rational, Rational)
+autoSteps nSteps (minV, maxV) = (min', max', step)
+  where
+    r@(minV', maxV')  | minV == maxV = (minV-0.5,minV+0.5)
+                      | otherwise    = (minV, maxV)
+    step = chooseStep (fromIntegral nSteps) r
+    min' = fromIntegral (floor   $ realToFrac minV' / step :: Integer) * step
+    max' = fromIntegral (ceiling $ realToFrac maxV' / step :: Integer) * step
+
+linearMap :: (Double, Double) -> (Double, Double) -> PointMap Double
+linearMap (l, u) (l', u') = PointMap mapFn (l, u)
+  where
+    mapFn x | x < l || x > u = Nothing
+            | otherwise = Just $ (x - l) / (u - l) * (u' - l') + l'
+{-# INLINE linearMap #-}
+
+linearMapBound :: (Double, Double) -> (Double, Double) -> PointMap Double
+linearMapBound (l, u) (l', u') = PointMap mapFn (l, u)
+  where
+    mapFn x | x < l = Just l' 
+            | x > u = Just u'
+            | otherwise = Just $ (x - l) / (u - l) * (u' - l') + l'
+{-# INLINE linearMapBound #-}
+
+hasNaN :: [(Double, Double)] -> Bool
+hasNaN = any (uncurry ((||) `on` isNaN))
+
+--text' :: Double -> String -> DiaR2
+--text' size str = stroke (textSVG' (TextOpts str lin2 INSIDE_WH HADV False size size)) # fc black # lwL 0
+
+--text' :: Double -> String -> DiaR2
+--text' size str = textVisualBounded (fontSize (Local size) mempty) str # fontSize (Local size)
+
+text' :: Double -> String -> DiaR2
+text' size str = text str # fontSizeL size
